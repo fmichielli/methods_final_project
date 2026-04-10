0caf582f53499738e5c1471e593b2a89ba1e8bc2
fmichielli <fcm2118@columbia.edu>
2026-04-09 21:29:08 -0400

    add poverty data and chapters 5 and 6

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ var config = {
             alignment: 'center',
             hidden: false,
             title: 'Small Businesses',
-            description: "Small businesses are a vital part of NYC's economy and community development. The blue dots represent the locations of small businesses across the city.",
+            description: "Small businesses are a vital part of NYC's economy and community development. The blue dots represent the locations of registered small businesses across the city.",
             location: {
                 center: [ -73.945036, 40.734321],
                 zoom: 11,
@@ -26,7 +26,7 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                 {
+                {
                  layer: 'sbs-points-basic',
                  opacity: 1,
                  duration: 3000
@@ -120,7 +120,7 @@ var config = {
             id: 'chapter-3',
             alignment: 'left',
             hidden: false,
-            title: 'Williamsburg and Greenpoint, Brooklyn, are a hub for women-owned businesses.',
+            title: 'Williamsburg and Greenpoint, Brooklyn, are hubs for women-owned businesses.',
             description: '',
             image: './legend_clean.png',
             location: {
@@ -214,6 +214,108 @@ var config = {
                    {
                        layer: 'census-data-poverty',
                        opacity: 0,
+                       duration: 3000
+                       }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'chapter-5',
+            alignment: 'center',
+            hidden: false,
+            title: "Poverty Rates Across NYC's Neighborhoods",
+            description: "According to New York officials, small businesses are the backbone of the city's economy, and have been critical in the New York City's recovery from the COVID pandemic. Let's investigate the relationship between small businesses and poverty rates across the city. The census data layers show the poverty rates across NYC's neighborhoods, with darker colors representing higher poverty rates.",
+            location: {
+                center: [-73.945036, 40.734321],
+                zoom: 10,
+                pitch: 0,
+                bearing: 0
+            }
+            ,mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'sbs-points-basic',
+                    opacity: 0,
+                    duration: 3000
+                    },
+                   {
+                       layer: 'sbs-points',
+                       opacity: 1,
+                       duration: 3000
+                       },
+                   {
+                       layer: 'sbs-points-business-cat',
+                       opacity: 0,
+                       duration: 3000
+                       },
+                   {
+                       layer: 'census-data-ethnicity',
+                       opacity: 0,
+                       duration: 3000
+                           },
+                   {
+                       layer: 'census-data-race',
+                       opacity: 0,
+                       duration: 3000
+                       },
+                  
+                   {
+                       layer: 'census-data-poverty',
+                       opacity: 1,
+                       duration: 3000
+                       }
+            ],
+            onChapterExit: []
+        },
+
+        {
+            id: 'chapter-6',
+            alignment: 'left',
+            hidden: false,
+            title: 'South Bronx',
+            description: 'The South Bronx is afflicted with high poverty rates, and, compared with neighboring Manhattan, has a relatively low density of small businesses. Nonetheless, minority business owners represent a significant portion of the small business community in the South Bronx.',
+            image: './legend_clean.png',
+            location: {
+                center: [-73.92743, 40.81341],
+                zoom: 13,
+                pitch: 0,
+                bearing: 0
+            }
+            ,mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {
+                    layer: 'sbs-points-basic',
+                    opacity: 0,
+                    duration: 3000
+                    },
+                   {
+                       layer: 'sbs-points',
+                       opacity: 1,
+                       duration: 3000
+                       },
+                   {
+                       layer: 'sbs-points-business-cat',
+                       opacity: 0,
+                       duration: 3000
+                       },
+                   {
+                       layer: 'census-data-ethnicity',
+                       opacity: 0,
+                       duration: 3000
+                           },
+                   {
+                       layer: 'census-data-race',
+                       opacity: 0,
+                       duration: 3000
+                       },
+                  
+                   {
+                       layer: 'census-data-poverty',
+                       opacity: 0.8,
                        duration: 3000
                        }
             ],
